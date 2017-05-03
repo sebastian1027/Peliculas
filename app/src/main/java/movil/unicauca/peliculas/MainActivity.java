f487841822dad9dc0f0f75fb41ac115f77b1d91b
Sebastian <sebastianl@unicauca.edu.co>
2017-05-02 20:38:29 -0500

    Creados los views de ingreso y registro

--- app/src/main/java/movil/unicauca/peliculas/MainActivity.java
+++ app/src/main/java/movil/unicauca/peliculas/MainActivity.java
@@ -1,13 +1,26 @@
 package movil.unicauca.peliculas;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import movil.unicauca.peliculas.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    public void goToNext(){
+     //String usr = binding.usuario.getEditText().getText().toString();
+     // String pass = binding.pass.getEditText().getText().toString();
+        Intent intent = new Intent(this, MoviesActivity.class);
+        startActivity(intent); //material design II
     }
 }
---
+++ app/src/main/java/movil/unicauca/peliculas/models/RegistroActivity.java
@@ -0,0 +1,15 @@
+package movil.unicauca.peliculas.models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import movil.unicauca.peliculas.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
