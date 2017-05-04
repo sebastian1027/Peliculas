a5e13df055d9a9b1d0b2171ab2275e6c6cd538f0
Sebastian <sebastianl@unicauca.edu.co>
2017-05-04 10:51:03 -0500

    este proyecto se encuentra en D/Moviels/ProyectosRepaso/Peliculas

--- app/src/main/java/movil/unicauca/peliculas/MoviesActivity.java
+++ app/src/main/java/movil/unicauca/peliculas/MoviesActivity.java
@@ -9,14 +9,17 @@ import android.view.LayoutInflater;
 import java.util.List;
 
 import movil.unicauca.peliculas.adapters.EstrenoAdapter;
+import movil.unicauca.peliculas.adapters.ProximoEstrenoAdapter;
 import movil.unicauca.peliculas.databinding.ActivityMoviesBinding;
 import movil.unicauca.peliculas.models.Estrenos;
 import movil.unicauca.peliculas.util.Data;
+import movil.unicauca.peliculas.util.Datape;
 
-public class MoviesActivity extends AppCompatActivity implements EstrenoAdapter.OnEstrenoSelected {
+public class MoviesActivity extends AppCompatActivity implements EstrenoAdapter.OnEstrenoSelected, ProximoEstrenoAdapter.OnProxEstrenoSelected {
 
     ActivityMoviesBinding binding;
     EstrenoAdapter adapter;
+    ProximoEstrenoAdapter adapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,22 @@ public class MoviesActivity extends AppCompatActivity implements EstrenoAdapter.
         adapter = new EstrenoAdapter(getLayoutInflater(), Data.getData(), this);
         binding.list.setAdapter(adapter);
         binding.list.setLayoutManager(new LinearLayoutManager(this));
+
+
+        adapter1 = new ProximoEstrenoAdapter(getLayoutInflater(), Datape.getDatape(), this);
+        binding.list.setAdapter(adapter1);
+        binding.list.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 
     @Override
     public void onEstreno(int position) {
+
+    }
+
+    @Override
+    public void onProxEstreno(int position) {
 
     }
 }
